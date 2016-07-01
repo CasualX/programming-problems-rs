4a0e327cbc2b0ab210f3b7c6af9d7886aa78c4e4
Jere K <Jere K>
2016-07-01 19:32:03 +0300

    Adds new line at EOF.

--- src/list, strings/9-concatenate.rs
+++ src/list, strings/9-concatenate.rs
@@ -3,17 +3,17 @@
 fn main() {
     let a_vec: Vec<i32> = vec![1, 2, 3, 4];
     let b_vec: Vec<i32> = vec![5, 6, 7];
- 
+
     let c_vec = concatenate_arrays::<i32>(&a_vec, &b_vec);
- 
+
     println!("{:?} ~ {:?} => {:?}", a_vec, b_vec, c_vec);
 }
- 
+
 fn concatenate_arrays<T: Clone>(x: &[T], y: &[T]) -> Vec<T> {
     let mut concat: Vec<T> = vec![x[0].clone(); x.len()];
- 
+
     concat.clone_from_slice(x);
     concat.extend_from_slice(y);
- 
+
     concat
 }
